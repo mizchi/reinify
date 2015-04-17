27e77a56b98ecfe3b15386628fe6bb80aedfaf7b
Koutarou Chikuba <miz404@gmail.com>
2015-04-18 06:07:02 +0900

    Init

---
+++ index.js
@@ -0,0 +1,21 @@
+(function() {
+  var path = require('path');
+  var fs = require('fs');
+  var through = require('through');
+  var reiny = require('reiny');
+
+  module.exports = function(file) {
+    if (!/\.reiny$/.test(file)) {
+      return through();
+    }
+    var inputString = '';
+    var write = function(buf) {
+      return inputString += buf;
+    };
+    var end = function() {
+      this.queue(reiny.compile(inputString));
+      return this.queue(null);
+    };
+    return through(write, end);
+  };
+})();
